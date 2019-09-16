c342d9e4ff6419fa31eff6735b21db08e6cf687b
rosanaemidio <rosanaemidio42@gmail.com>
2019-09-16 16:46:01 -0300

    exerecício 3 respondido

--- Aula-01/01-Exercicios.js
+++ Aula-01/01-Exercicios.js
@@ -1,17 +1,20 @@
 // Exercicios 01
 
 // Declare uma variável constante com um nome válido
-
+const nomeUsuario
 // Mostre o conteúdo dessa variável
-
+const nomeUsuario = "rosana"
+console.log(typeof nomeUsuario)
+return;
 // Atribua um valor arbitrário à essa variável
+nomeUsuario=14
 
 // Mostre um alerta na tela utilizando o conteúdo dessa variável
-
+alert("digite seu nome")
 // Declare uma variável(let) chamada 'mensagem' e atribua o valor "Olá" a essa variável
-
+let mensagem= "Olá"
 // Declare uma variável(let) chamada 'nome' e atribua a ela o valor informado pelo(a) usuário(a)
-
+let nome="rosana" 
 // Mostre na tela do navegador a mensagem composta pela junção das variáveis 'mensagem' e 'nome'
 
 // Sabe aquela variável que você criou? Tente atribuir um novo valor à ela
--- Aula-01/03-Exercicios.js
+++ Aula-01/03-Exercicios.js
@@ -92,3 +92,106 @@
 // !""
 
 // "Come"+ " " + "Abacate!"
+
+// ===== *RESPOSTAS* ======
+
+let x = 2
+undefined
+let y = 5
+undefined
+let z = 15
+undefined
+x+y-z
+-8
+x/y
+0.4
+y*z
+75
+x**y+z/y
+35
+z//y
+15
+z%x
+1
+(x+y)*(z-x)/8
+11.375
+3*x+5*y+0.5*4
+33
+x=y+z
+20
+7*x
+140
+let r1= 58
+undefined
+let r2= 18/5
+undefined
+2*3.18*r1
+368.88
+2*3.18*r2
+22.896
+let a=1
+undefined
+let b=2
+undefined
+let c=3
+undefined
+(18<2*5)
+false
+a>b&&a>c
+false
+Math.PI
+3.141592653589793
+2*Math.PI*r1
+364.424747816416
+2*Math.PI*r2
+22.61946710584651
+a<c
+true
+a===c-b
+true
+a>=b
+false
+b**a>b*c
+false
+a++
+1
+++a
+3
+a+=2
+5
+a++>b++||a-->0
+true
+b+a+"nana"+c+b
+"9nana33"
+"Ana">"ana"
+false
+"ANA"!="Ana"
+true
+"zeca">"ana"
+true
+"ANA">"Ana"
+false
+3*'Ana'
+NaN
+'5'===5
+false
+'5'==5
+true
+5===25/5
+true
+4!==3
+true
+"4"!==3
+true
+true==0
+false
+false==0
+true
+!!5
+true
+!0
+true
+!""
+true
+"Come"+ " " +"Abacate!"
+"Come Abacate!"
